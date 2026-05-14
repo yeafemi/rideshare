28c68c5aa947686494feb9782c072f582b0f2419
yeafemi <yeafemi@example.com>
2026-05-14 18:36:50 +0000

    fix: use dynamic basepath in router

--- src/router.tsx
+++ src/router.tsx
@@ -10,7 +10,7 @@ export const getRouter = () => {
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
-    basepath: "/",
+    basepath: import.meta.env.BASE_URL,
   });
 
   return router;
